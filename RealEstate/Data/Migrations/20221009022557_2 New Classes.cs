86252d779db71fc7a4d56aa67f18d0e1ac37db5d
Ethan Curtis <95894855+UltraSpeedDemon@users.noreply.github.com>
2022-10-09 00:38:29 -0400

    Site Modifications

---
+++ RealEstate/Data/Migrations/20221009022557_2 New Classes.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstate.Data.Migrations
+{
+    public partial class _2NewClasses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AreaCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.CityId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForSale",
+                columns: table => new
+                {
+                    ForSaleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2500)", maxLength: 2500, nullable: false),
+                    Photo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rooms = table.Column<int>(type: "int", nullable: false),
+                    SqFootage = table.Column<int>(type: "int", nullable: false),
+                    CityId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CityId1 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForSale", x => x.ForSaleId);
+                    table.ForeignKey(
+                        name: "FK_ForSale_City_CityId1",
+                        column: x => x.CityId1,
+                        principalTable: "City",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForSale_CityId1",
+                table: "ForSale",
+                column: "CityId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ForSale");
+
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
