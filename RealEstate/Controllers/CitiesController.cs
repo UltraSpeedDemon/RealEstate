aa8021f4c6fcac13f8c0c66b5fbb510ca9daacf6
Ethan Curtis <95894855+UltraSpeedDemon@users.noreply.github.com>
2022-11-16 22:07:52 -0500

    Login, Authentication and Google Login implemented

--- RealEstate/Controllers/CitiesController.cs
+++ RealEstate/Controllers/CitiesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using RealEstate.Models;
 
 namespace RealEstate.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class CitiesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,12 +22,14 @@ namespace RealEstate.Controllers
         }
 
         // GET: Cities
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
               return View(await _context.Cities.ToListAsync());
         }
 
         // GET: Cities/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Cities == null)
